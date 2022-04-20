3f246c794dedaf6e9b03723b63ac73a0c1c80764
Enric Mieza <emieza@xtec.cat>
2022-04-20 20:01:10 +0200

    pluralize i indent spaces

--- vota/models.py
+++ vota/models.py
@@ -3,12 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Votacio(models.Model):
-	titol = models.CharField(max_length=255)
-	desc = models.TextField(blank=True,null=True)
+    class Meta:
+        verbose_name_plural = "Votacions"
+    titol = models.CharField(max_length=255)
+    desc = models.TextField(blank=True,null=True)
 
 class Opcio(models.Model):
-	titol = models.CharField(max_length=255)
-	desc = models.TextField(blank=True,null=True)
-	votacio = models.ForeignKey(Votacio, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = "Opcions"
+    titol = models.CharField(max_length=255)
+    desc = models.TextField(blank=True,null=True)
+    votacio = models.ForeignKey(Votacio, on_delete=models.CASCADE)
 
 
